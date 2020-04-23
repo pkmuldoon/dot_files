8fcf43d71eb34485a4e703819bf38f41fc8828df
Phil Muldoon <phillip.muldoon@freeagent.com>
2020-04-23 17:11:55 +0100

    Use fd for fzf. Move starship to end of file.

--- fish/config.fish
+++ fish/config.fish
@@ -7,8 +7,8 @@ set -gx GOPATH $HOME/.go
 set -gx PATH /usr/local/opt/qt@5.5/bin:/usr/local/bin:$HOME/bin:$HOME/usr/local/bin:/Users/phillipmuldoon/.cask/bin:$GOPATH/bin:$GOROOT/bin $PATH
 set -gx NVM_DIR $HOME/.nvm
 
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
+#source /usr/local/share/chruby/chruby.fish
+#source /usr/local/share/chruby/auto.fish
 
 alias rsp='bundle exec rspec'
 alias rke='bundle exec rails'
@@ -20,6 +20,7 @@ alias emacs="/usr/local/opt/emacs-plus/Emacs.app/Contents/MacOS/Emacs"
 set -gx EDITOR emacs
 set -gx VISUAL $EDITOR
 
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(emacs {})+abort'
 --color=dark
 --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
@@ -27,8 +28,12 @@ export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(emacs {})+abort'
 
 defaults write -g com.apple.mouse.scaling  6.0
 
-starship init fish | source
-
 export DIRENV_LOG_FORMAT=
 direnv hook fish | source
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+# Increase number of open files to appease chromedriver
+ulimit -n 2056
+
+starship init fish | source
+
